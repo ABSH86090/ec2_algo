637d0dc2d50ce182799503f730f7d6833c4d44ba
ABHISHEK SHARMA <absh86090@gmail.com>
2026-02-03 15:20:25 +0530

    Update nifty_supertrend_spread.py

--- nifty_supertrend_spread.py
+++ nifty_supertrend_spread.py
@@ -194,7 +194,7 @@ def get_nifty_option_symbols(fyers):
 def is_today_nifty_expiry():
     return datetime.date.today() == get_next_tuesday_expiry()
 
-LOTS = 10 if datetime.date.today() == get_next_tuesday_expiry() else 16
+LOTS = 8 if datetime.date.today() == get_next_tuesday_expiry() else 12
 QTY = LOT_SIZE * LOTS
 send_telegram(
     f"📦 LOT CONFIG | "
